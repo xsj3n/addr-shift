57835225adc89f7c3502a1fc780b4f8d4f6c7da2
xsj3n <xsj3n@tutanota.com>
2025-11-26 17:37:54 -0700

    email instead of author, attempt env prepend

--- report.sh
+++ report.sh
@@ -11,8 +11,14 @@ echo "$ip" > "${ip_enc_file:0:-4}"
 gpg --encrypt --recipient-file "./recipient.asc" --yes --trust-model always --output "$ip_enc_file" "${ip_enc_file:0:-4}"
 
 user=$(cat ./user.txt)
+email=$(cat ./email.txt)
 helper="!echo 'username=$user'; echo 'password=$(cat api.key)'";
+
 git add "$ip_enc_file"
-git commit -m "sync" --author="$user <$(cat ./author.txt)>"
+GIT_AUTHOR_NAME="$user" \
+GIT_COMMITER_NAME="$user" \
+GIT_AUTHOR_EMAIL="$email" \
+GIT_COMMITER_EMAIL="$email" \
+git commit -m "sync"
 git -c credential.helper="$helper" push --force
 rm "$ip_enc_file"
